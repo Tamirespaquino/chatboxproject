9d70f7ba61008ba225c98e3295b40ecfc6d10295
Tamirespaquino <tamirespaquino@gmail.com>
2022-09-21 09:37:56 -0300

    DLQ configuration

--- Consumer/src/main/java/com/tamiresntt/consumer/config/RabbitMQConfig.java
+++ Consumer/src/main/java/com/tamiresntt/consumer/config/RabbitMQConfig.java
@@ -1,5 +1,8 @@
 package com.tamiresntt.consumer.config;
 
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +10,27 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-
     @Bean
     public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
+
+   @Bean
+    DirectExchange deadLetterExchange() {
+        return new DirectExchange("deadLetterExchange");
+   }
+
+   @Bean
+    DirectExchange exchange() {
+        return new DirectExchange("javainuseExchange");
+   }
+
+   @Bean
+   Queue dlq() {
+        return QueueBuilder.durable("deadLetter.queue").build();
+   }
+
+   @Bean
+    Queue queue
+
 }
